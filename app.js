c806af34f01791561cbd620ae051d2b45e5561d0
rhynecs <contact@henrystevens.me>
2022-07-12 10:21:32 +1000

    added cors and json middleware, added nodemon to package file

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ dotenv.config();
 
 const express = require("express");
 const app = express();
+const cors = require("cors")
 const apiRoutesV1 = require("./routes")
 
-
+// allow cross-origin resource sharing
+app.use(cors())
+// interpret request body as json
+app.use(express.json())
 app.use("/api/v1/", apiRoutesV1)
+
 
 // export app to index.js to run
 module.exports = app;
