ec6b06a8592284b1631c32324e280dafe239d541
rhynecs <contact@henrystevens.me>
2022-07-15 15:09:52 +1000

    created admin authenticator middleware - tested and applied to topic post route

---
+++ middleware/admin_auth.js
@@ -0,0 +1,13 @@
+// must be called after jwt_authorize middleware
+const authorizeAdmin = (req, res, next) => {
+    if (!req?.role) {
+        return res.status(401).send({ error: "user role not present" })
+    }
+
+    if (req.role !== "admin") {
+        return res.status(401).send({ error: "must be an admin" })
+    }
+    next()
+}
+
+module.exports = authorizeAdmin
